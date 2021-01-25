e3a201e835c7e5b0fae51abdd6a0f355ebf64ee7
Tan Zhong Ming <mingblog0520@gmail.com>
2021-01-25 15:46:23 +0800

    Update FPGrowth.py - remove all comment

--- FPGrowth.py
+++ FPGrowth.py
@@ -65,38 +65,12 @@ class FPGrowth:
     def generateItemTable(self):
         itemTbl = []
         for basicItem in self.basicItem:
-#             print(f'===={basicItem}====')
             mask = np.isin(self.rawSet,basicItem)
             vec_mask = np.isin(mask.sum(axis=1), [1])
             idxs = np.where(vec_mask)
-#             print(f'{idxs[0]} => {len(idxs[0])}')
             count = len(idxs[0])
             itemTbl.append(ItemTable(basicItem,count,idxs[0]))
         return itemTbl
-    
-#     def calculateMinSuppCount(self,basicItem):
-#         #https://stackoverflow.com/questions/56419519/selecting-rows-of-numpy-array-that-contains-all-the-given-values
-#         item = basicItem
-        
-#         if(type(item) == np.str):
-#             k = 1
-#         elif(type(item) == np.ndarray):
-#             k = len(item)
-#         else:
-#             k = len(item)
-#         mask = np.isin(self.rawSet,item)
-#         vec_mask = np.isin(mask.sum(axis=1), [k])
-#         ids = np.where(vec_mask)
-#         minSuppCount = len(ids[0])
-#         return minSuppCount
-            
-#     def generateItemTable(self,basicItemset):
-#         itemTables = []
-#         for basicItem in basicItemset:
-#             suppCount = self.calculateMinSuppCount(basicItem)
-#             itemtable = ItemTable(basicItem,suppCount)
-#             itemTables.append(itemtable)
-#         return itemTables
     def reverseName(self,itemTables):
         alphaDsc = sorted(itemTables, key=lambda x: x.name,reverse=True)
         return sorted(alphaDsc, key=lambda x: x.count)
@@ -113,12 +87,10 @@ class FPGrowth:
 
     def filterItemTable(self,itemTables):
 #         https://stackoverflow.com/questions/7623715/deleting-list-elements-based-on-condition
-#         return filter(lambda x: x.count >= self.minSuppCount, itemTables)
         return [item for item in itemTables if item.count >= self.minSuppCount]
 
     def getItemTable(self):
         itemTbl = self.generateItemTable()
-#         itemTbl = self.filterItemTable(itemTbl)
         itemTbl = self.getDscItemTable(itemTbl)
         return itemTbl
     
@@ -159,9 +131,7 @@ class FPGrowth:
         currentNode = root
         for i,row in enumerate(self.rawSet):
             currentNode = root
-        #     print(f'===={row}====')
             maxDepth = len(row[row != None])
-        #     print(f'{maxDepth}')
             for basicItem in itemTbl:
                 mask = np.isin(basicItem.idxs,i)
                 occurance = mask.sum()
@@ -183,14 +153,11 @@ class FPGrowth:
     def generateCPBase(self,itemTbl):
         for item in itemTbl:
             pathList = []
-        #     print(f'====={item.name}======')
             nodes = item.head
             for node in nodes:
                 paths = []
-        #         print('-----')
                 for path in node.path:
                     if(path.name != 'null' and path.name!=item.name):
-        #                 print(path.name)
                         paths.append(path.name)
                 if(len(paths) > 0):
                     pathList.append(CPB(paths,path.count))
@@ -210,17 +177,13 @@ class FPGrowth:
             root = Node('null')
             currentNode = root
             CFPTreeTable = []
-#             print(f'===={item.name}====')
             cpbs = item.CPBase
             basicItemTable = []
             for cpb in cpbs:
                 currentNode = root
                 maxDepth = len(cpb.path)
-#                 print(f'maxDepth=> {maxDepth}')
-#                 print(f'{cpb.path}=>{cpb.count}')
                 for itemName in cpb.path:
                     index = self.searchBasicItemTable(basicItemTable,itemName)
-#                     print(f'{index}  {itemName}')
                     if(index == -99):
                         basicItem = ItemTable(itemName,cpb.count)
                     else:
@@ -235,48 +198,15 @@ class FPGrowth:
             item.CFPTreeTable = basicItemTable
             item.CFPTree = root
         return itemTbl
-    
-    
-    
-#     def generateCFPTree(self,itemTbl):
-#         for item in itemTbl:
-#             root = Node('null')
-#             currentNode = root
-#             CFPTreeTable = []
-# #             print(f'===={item.name}====')
-#             cpbs = item.CPBase
-#             basicItemTable = []
-#             for cpb in cpbs:
-#                 currentNode = root
-#                 maxDepth = len(cpb.path)
-# #                 print(f'maxDepth=> {maxDepth}')
-# #                 print(f'{cpb.path}=>{cpb.count}')
-#                 for itemName in cpb.path:
-#                     basicItem = ItemTable(itemName,cpb.count)
-#                     currentNode,basicItem = self.updateTree(currentNode,basicItem,maxDepth,cpb.count)
-#                     basicItemTable.append(basicItem)
-#             item.CFPTreeTable = basicItemTable
-#             item.CFPTree = root
-#         return itemTbl
     def pruneBranchTree(self,itemTbl):
         # prune branch that doesnt meet min supprt count
         # https://buildmedia.readthedocs.org/media/pdf/anytree/latest/anytree.pdf
         # 2.3 Detach/Attach Protocol page 8
         # c.parent=None
         for item in itemTbl:
-#             print(f'======{item.name}======\n')
-#             print(f'---Tree---')
-#             print(RenderTree(item.CFPTree))
-#             print('\n')
             for node in item.CFPTree.descendants:
-#                 currentNode = newTree
-        #         print(node)
                 if node.count < self.minSuppCount:
-#                     print(f'prune {node.name}')
                     node.parent = None
-#             print('\n')
-#             print(RenderTree(item.CFPTree))
-#             print('===\n')
         return itemTbl
 
     def removeBranchTable(self,itemTbl):
@@ -286,54 +216,30 @@ class FPGrowth:
         # Using del
         # del array[index]
         for item in itemTbl:
-#             print(f'======{item.name}======\n')
-#             print(f'---Tree---')
-#             print(RenderTree(item.CFPTree))
-#             print(f'\n---NodeLink---\n')
             for basicItem in item.CFPTreeTable: 
-        #         if(len(basicItem.head) <= 0):
-        #             print(f'{basicItem.name} is empty')
-        #             continue
-#                 print(f'{basicItem.name}=>{basicItem.head}')
                 for index,nodeLink in enumerate(basicItem.head):
                     if(nodeLink.root != item.CFPTree.root):
-#                         print(f'Remove {nodeLink.name}')
                         del basicItem.head[index]
-#                     else:
-#                         print(f'Node:{nodeLink}=>{nodeLink.is_leaf}=>{nodeLink.root}')
-#                 print('\n')
-#             print('===\n')
         return itemTbl
 
     def generateConditionalPattern(self,itemTbl):
     # generate frequent pattern
         for item in itemTbl:
-#             print(f'======{item.name}======\n')
-#             print(f'---Tree---')
-#             print(RenderTree(item.CFPTree))
-#             print(f'\n---NodeLink---\n')
             condPats = []
             for basicItem in item.CFPTreeTable:
                 if(len(basicItem.head) <= 0):
-#                     print(f'{basicItem.name} is empty')
                     continue
-        #         print(f'{basicItem.name}=>{basicItem.head}')
                 for index,nodeLink in enumerate(basicItem.head):
                     condPatsItem = {}
-#                     print(f'{nodeLink} => {nodeLink.is_leaf} => {nodeLink.root}')
                     currentNode = nodeLink
                     if(not nodeLink.is_leaf):
                         continue
                     while(currentNode.parent):
                         condPatsItem[currentNode.name] = currentNode.count
-#                         print(f'Append {currentNode.name} , {currentNode.count}')
                         currentNode = currentNode.parent
-#                     print(condPatsItem)
                     if condPatsItem:
                         condPats.append(condPatsItem)
             item.CFP = condPats
-#             print(item.FP)
-#             print('\n')
         return itemTbl
 
     # https://www.youtube.com/watch?v=9oPNGofa1pI&t=32s
@@ -349,25 +255,17 @@ class FPGrowth:
         return subSets
 
     def generateFrequentPattern(self,cfp,baseItem):
-    #     print('Start generate frequent pattern')
         frequentPatterns = []
         CFPItems = []
-    #     print(f'Before reverse: {cfp}')
         cfp = OrderedDict(reversed(list(cfp.items()))) 
-    #     print(f'After reverse: {cfp}')
         for key,value in cfp.items():
             CFPItems.append(key)
-    #         print(cfp[key])
 
         subsets = self.powerset(CFPItems)
 
         for subset in subsets:
             frequentPattern = ((subset+[baseItem],cfp[subset[-1]]))
             frequentPatterns.append(frequentPattern)
-
-    #     print('FrequentPattern')
-    #     print(frequentPatterns)
-    #     print('End generate frequent pattern')
         return frequentPatterns
 
     def generateFrequentPatternTable(self,itemTbl):
@@ -375,13 +273,9 @@ class FPGrowth:
         for item in itemTbl:
             if(len(item.CFP) <= 0):
                 continue
-        #     print(f'==={item.name}===')
             frequentPatterns = []
             for cfp in item.CFP:
-        #         print(cfp)
                 frequentPatterns+=(self.generateFrequentPattern(cfp,item.name))
-        #     print(frequentPatterns)
-        #     print('----')
             for i,frequentPattern1 in enumerate(frequentPatterns):
                 for j in range(i+1,len(frequentPatterns)-1):
                     frequentPattern2 = frequentPatterns[j]
@@ -391,9 +285,7 @@ class FPGrowth:
                         newTup = (newName,newCount)
                         frequentPatterns[i] = newTup
                         del frequentPatterns[j]
-        #                 print(f'{frequentPattern1} same as {frequentPattern2}')
             item.FP = frequentPatterns
-        #     print('\n')
         return itemTbl
     
     def calculateMinSuppCount(self,basicItem):
@@ -421,22 +313,18 @@ class FPGrowth:
             print(f'Largest k is {largestK}')
 
         for item in itemTbl:
-#             print(f'==={item.name}===')
             frequentPatterns = item.FP
             for frequentPattern in frequentPatterns:
                 itemset = frequentPattern[0]
                 ln = frequentPattern[1]
                 if(len(itemset)!=largestK and isLargestK):
                     continue
-#                 print(frequentPattern)
                 subsets = self.powerset(itemset,1)
                 item
-#                 print(f'subsets: {subsets}')
                 for subset in subsets:
                     s = subset
                     l = np.setdiff1d(itemset,subset)
                     sn = self.calculateMinSuppCount(s)
-#                     print(f'{s}=>{l} confidence = {ln}/{sn} = {ln/sn}')
                     associationRule = AssociationRule(s,l,sn,ln)
                     item.StrongRule.append(associationRule)
         return itemTbl
@@ -500,37 +388,24 @@ class FPGrowth:
                 print(f'{s}=>{l} confidence = {ln}/{sn} = {confidence}')
                 
     def to_df(self,itemTbl):          
-#         print('Item|||Conditional Pattern Base|||Conditional FP-tree')
-#         maxLen = 0
         data = {}
         data['Item'] = []
         data['CPB'] = []
         data['CFP'] = []
         data['FP'] = []
         for item in itemTbl:
-#             print(item.name,end='  |||')
             data['Item'].append(item.name)
             conditionalPatternBase = []
             for cpb in item.CPBase:
-#                 msg = f"{{{cpb.path}:{cpb.count}}}"
-#                 maxLen = max(maxLen,len(msg))
-#                 print(msg,end=', ')
                 dicted = (cpb.path,cpb.count)
                 conditionalPatternBase.append(dicted)
 
-#             print('|||',end='')
             conditionalFPTree = []
-        #     for uniqueItem in item.CFPTreeTable:
-        #         print(uniqueItem.name,end=',')
-        #         for path in uniqueItem.
             for cfp in item.CFP:
-#                 print(cfp,end=',')
                 conditionalFPTree.append(cfp)
-#             print('\t',end='')
             data['FP'].append(item.FP)
             data['CFP'].append(conditionalFPTree)
             data['CPB'].append(conditionalPatternBase)
-#             print('\n')
         df = pd.DataFrame(data)
         return df
     
@@ -576,7 +451,4 @@ class FPGrowth:
         self.displayStrongRule(itemTbl)
         print('========================================================================')
         df = self.to_df(itemTbl)
-        # pd.set_option("display.max_rows", None, "display.max_columns", None,"max_colwidth", None)
-        # print('Display all in one:')
-        # print(df)
         return df
